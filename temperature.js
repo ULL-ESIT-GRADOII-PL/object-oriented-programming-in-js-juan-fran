311992e372aee17083b375a71c9389ada7869896
Franjpr <alu0100503623@ull.edu.es>
2016-03-02 20:53:24 +0000

    Inicializar clases js

---
+++ temperature.js
@@ -0,0 +1,32 @@
+"use strict"
+
+function Medida(val,tipo){
+    var valor;
+    var tipo;
+
+
+}
+
+function Temperatura(){
+  Medida.call(this);
+}
+
+//Temperatura hereda de medida
+Temperatura.prototype = Object.create(Medida.prototype);
+
+function Celcius(){
+  Temperatura.call(this);
+}
+
+function Farenheit(){
+
+}
+
+function Kelvin(){
+
+}
+
+//C, F y K heredan de Temperatura
+Celcius.prototype = Object.create(Temperatura.prototype);
+Farenheit.prototype = Object.create(Temperatura.prototype);
+Kelvin.prototype = Object.create(Temperatura.prototype);
